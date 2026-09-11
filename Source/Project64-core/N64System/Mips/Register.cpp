danballweg/project64

commit 456f25eb6b341bc8c0415aea66938cbd4ba4f45c

Core: Get DoIntrException to use TriggerException

Source/Project64-core/N64System/Mips/Register.cpp
@@ -673,42 +673,11 @@ void CRegisters::DoCopUnusableException(bool DelaySlot, int32_t Coprocessor)
 
 bool CRegisters::DoIntrException(bool DelaySlot)
 {
-    if ((STATUS_REGISTER & STATUS_IE) == 0)
+    if ((STATUS_REGISTER & STATUS_IE) == 0 || (STATUS_REGISTER & STATUS_EXL) != 0 || (STATUS_REGISTER & STATUS_ERL) != 0)
     {
         return false;
     }
-
-    if ((STATUS_REGISTER & STATUS_EXL) != 0)
-    {
-        return false;
-    }
-
-    if ((STATUS_REGISTER & STATUS_ERL) != 0)
-    {
-        return false;
-    }
-
-    if (GenerateLog() && LogExceptions() && !LogNoInterrupts())
-    {
-        LogMessage("%08X: Interrupt generated", m_PROGRAM_COUNTER);
-    }
-
-    CAUSE_REGISTER.ExceptionCode = EXC_INT;
-    CAUSE_REGISTER.CoprocessorUnitNumber = 0;
-
-    if (DelaySlot)
-    {
-        CAUSE_REGISTER.BranchDelay = 1;
-        EPC_REGISTER = (int64_t)((int32_t)m_PROGRAM_COUNTER - 4);
-    }
-    else
-    {
-        CAUSE_REGISTER.BranchDelay = 0;
-        EPC_REGISTER = (int64_t)((int32_t)m_PROGRAM_COUNTER);
-    }
-
-    STATUS_REGISTER |= STATUS_EXL;
-    m_PROGRAM_COUNTER = 0x80000180;
+    TriggerException(EXC_INT, 0);
     return true;
 }
 

commit 059db162de7035db9f705c70686bb40856cbea4f

Core: Clear pipeline stage before triggering an interrupt exception
Fixes Mario Kart 64 hanging after boot (regressed in 456f25e)

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

Source/Project64-core/N64System/Mips/Register.cpp
@@ -777,6 +777,8 @@ bool CRegisters::DoIntrException()
     {
         return false;
     }
+    // Interrupt is not in a delay slot, ignore any stale pipeline stage
+    m_System.m_PipelineStage = PIPELINE_STAGE_NORMAL;
     TriggerException(EXC_INT, 0);
     return true;
 }